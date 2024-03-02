e43209f04506c8680d6dcd75332f226dbb97f4ce
Yana <y.kozhev@yandex.ru>
2024-03-02 12:37:42 +0700

    init update

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HomeModule } from './home/home.module';
+import appConfig from './config/app/app.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+      envFilePath: ['.env'],
+    }),
+    HomeModule,
+  ],
+})
+export class AppModule {}
