eabebc833704a5f92fb7493d2be5026c639caace
Yana <y.kozhev@yandex.ru>
2024-04-03 21:06:59 +0700

    update data

--- src/app/modules/business/business.module.ts
+++ src/app/modules/business/business.module.ts
@@ -5,9 +5,23 @@ import { ArmorEntity } from './entity/armor.entity';
 import { WeaponEntity } from './entity/weapon.entity';
 import { BusinessService } from './business.service';
 import { ArmorClassEntity } from '../characters/entity/armor.class.entity';
+import { ClothesEntity } from './entity/clothes.entity';
+import { HouseEnity } from './entity/house.entity';
+import { ItemEnity } from './entity/item.entity';
+import { ToolKitEnity } from './entity/toolkit.entity';
+import { VehicleEntity } from './entity/vehicle.entity';
 @Module({
     imports: [
-        TypeOrmModule.forFeature([ArmorEntity, ArmorClassEntity, WeaponEntity]),
+        TypeOrmModule.forFeature([
+            ArmorEntity,
+            ArmorClassEntity,
+            ClothesEntity,
+            HouseEnity,
+            ItemEnity,
+            ToolKitEnity,
+            VehicleEntity,
+            WeaponEntity,
+        ]),
     ],
     controllers: [BusinessController],
     providers: [BusinessService],
